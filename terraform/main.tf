742b1906028f210f429a438ad806ca231724f3d7
Denis <denis.lavrushko@infobip.com>
2021-04-25 19:39:58 +0300

    new tf module version

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {}
 }
 
-module "webserver_cluster" {
+module "tf-module-workadventure" {
   source                          = "github.com/behoof4mind/tf-module-workadventure?ref=0.0.5"
   eks_region                      = "us-east-2"
   eks_cluster_version             = "1.18"
--- terraform/output.tf
+++ terraform/output.tf
@@ -1,27 +1,27 @@
 output "cluster_id" {
-  value       = module.webserver_cluster.cluster_id
+  value       = module.tf-module-workadventure.cluster_id
 }
 
 output "cluster_endpoint" {
-  value       = module.webserver_cluster.cluster_endpoint
+  value       = module.tf-module-workadventure.cluster_endpoint
 }
 
 output "cluster_security_group_id" {
-  value       = module.webserver_cluster.cluster_security_group_id
+  value       = module.tf-module-workadventure.cluster_security_group_id
 }
 
 output "kubectl_config" {
-  value       = module.webserver_cluster.kubectl_config
+  value       = module.tf-module-workadventure.kubectl_config
 }
 
 output "config_map_aws_auth" {
-  value       = module.webserver_cluster.config_map_aws_auth
+  value       = module.tf-module-workadventure.config_map_aws_auth
 }
 
 output "region" {
-  value       = module.webserver_cluster.region
+  value       = module.tf-module-workadventure.region
 }
 
 output "cluster_name" {
-  value       = module.webserver_cluster.cluster_name
+  value       = module.tf-module-workadventure.cluster_name
 }
